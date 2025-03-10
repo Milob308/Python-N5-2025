67fae4a734dac7d046da7723a8309bcc26ec6ff1
Milo Birkenshaw <M.Birkenshaw@albynschool.org>
2025-03-10 09:58:14 +0000

    hi

---
+++ task 4.py
@@ -0,0 +1,7 @@
+age = float(input("please input your age:"))
+if age <0.0:
+    print("not possible")
+elif age < 17.0:
+    print("you cannot learn to drive yet")
+else:
+    print("you can learn to drive")
