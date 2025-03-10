eb5550ecbd8823cb6ed7c108786d36a5fa338bd0
Milo Birkenshaw <M.Birkenshaw@albynschool.org>
2025-03-10 09:16:43 +0000

    hello

---
+++ task 2.py
@@ -0,0 +1,6 @@
+username = input("please enter your username:")
+password = input("Please enter your password:")
+if username == "ian" and password == "letmein":
+    print("logon accepted")
+else:
+    print("password incorrect")
