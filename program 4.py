407b6234e42ac7ccd696842ee470ddeb163c1bc2
Milo Birkenshaw <M.Birkenshaw@albynschool.org>
2025-03-12 15:35:19 +0000

    hi

---
+++ program 4.py
@@ -0,0 +1,9 @@
+rightnumber = 8
+guess = 1
+number = int(input("Guess a number: "))
+while (guess < 5) and (number != rightnumber):
+  print("Not right. Try again.")
+  number = int(input("Guess a number: "))
+  guess = guess + 1
+if (number == 8):
+  print("Well Done!")
---
+++ scoreboard project.py
@@ -0,0 +1,8 @@
+home = 0
+away = 0
+period = 1
+
+while period <= 3:
+    print("")
+    print("Home-",home,"-",away,"- Away")
+    
