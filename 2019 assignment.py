e6f93c960506758cbd024a37d7f2572c4d83afa3
Milo Birkenshaw <M.Birkenshaw@albynschool.org>
2025-12-15 11:27:47 +0000

    hi

---
+++ 2019 assignment.py
@@ -0,0 +1,13 @@
+import random
+
+endings = ["ing","end","axe","gex","goh"]
+NumStudents = int(input("please enter the number of students: "))
+for index in range(NumStudents):
+    name = input("please enter the first three letters of the student's names: ")
+    while len(name) != 3:
+        print("invalid name")
+        name = input("please enter the first three letters of the student's names: ")
+    number = random.randint(0,4) 
+    username = (name + endings[number])
+    print("your username is,",username)
+    
